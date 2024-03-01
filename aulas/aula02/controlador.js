273cd6bf76ed56a43771564aff0076d7a2aad808
Jose Reginaldo <prof.josereginaldo@gmail.com>
2024-03-01 20:26:22 -0300

    codigo da aula2

---
+++ aulas/aula02/controlador.js
@@ -0,0 +1,47 @@
+const readline = require("readline-sync");
+
+const Produto = require('./modelo');
+
+const produtos = [];
+
+function listar() {
+  produtos.forEach((produto) => console.log(produto.toString()));
+}
+
+function criar(nome, preco) {
+  const novo = new Produto(nome, preco);
+  produtos.push(novo);
+}
+
+function buscar() {
+  const nome = readline.question("Entre com o nome do produto: ");
+  const buscou = produtos.find((produto) => produto.nome === nome);
+  if (buscou) {
+    console.log(buscou.toString());
+  } else {
+    console.log("Produto não localizado");
+  }
+}
+
+function atualizar() {
+  const nome = readline.question("Entre com o nome do produto: ");
+  const buscou = produtos.find((produto) => produto.nome === nome);
+  if (buscou) {
+    const preco = readline.question("Entre com o novo preco: ");
+    buscou.preco = preco;
+  } else {
+    console.log("Produto não localizado");
+  }
+}
+
+function remover() {
+  const nome = readline.question("Entre com o nome do produto: ");
+  const posicao = produtos.findIndex((produto) => produto.nome === nome);
+  if (posicao >= 0) {
+    produtos.splice(posicao, 1);
+  } else {
+    console.log("Produto não localizado");
+  }
+}
+
+module.exports = { criar, listar, buscar, atualizar, remover };
